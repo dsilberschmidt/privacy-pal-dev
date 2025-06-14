9124723dbd6518d7dcc3572cb0a67cf4680b2b7e
Daniel Silberschmidt <daniel.silberschmidt@gmail.com>
2025-06-14 15:49:19 +0200

    feat: integrar IndexedDB para guardar y mostrar políticas

---
+++ db.js
@@ -0,0 +1,39 @@
+const DB_NAME = 'PrivacyPalDB';
+const DB_VERSION = 1;
+const STORE_NAME = 'policies';
+
+function openDB(callback) {
+  const request = indexedDB.open(DB_NAME, DB_VERSION);
+
+  request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    if (!db.objectStoreNames.contains(STORE_NAME)) {
+      db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true });
+    }
+  };
+
+  request.onsuccess = () => callback(null, request.result);
+  request.onerror = () => callback(request.error, null);
+}
+
+function savePolicy(policy, callback) {
+  openDB((err, db) => {
+    if (err) return console.error(err);
+    const tx = db.transaction(STORE_NAME, 'readwrite');
+    tx.objectStore(STORE_NAME).add(policy);
+    tx.oncomplete = () => callback && callback(null);
+    tx.onerror = (e) => callback && callback(e);
+  });
+}
+
+function getAllPolicies(callback) {
+  openDB((err, db) => {
+    if (err) return console.error(err);
+    const tx = db.transaction(STORE_NAME, 'readonly');
+    const store = tx.objectStore(STORE_NAME);
+    const request = store.getAll();
+
+    request.onsuccess = () => callback(null, request.result);
+    request.onerror = () => callback(request.error, null);
+  });
+}
